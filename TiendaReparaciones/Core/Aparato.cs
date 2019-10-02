966971abee709f9d633a3d169131e4bac589d15c
fufer <fufer@DESKTOP-5DTO743>
2019-10-02 14:28:30 +0200

    Revert "Revert "Subir proyecto""
    This reverts commit 54496788eb8058964f3756c96679a0b96b8f21bd.

---
+++ TiendaReparaciones/Core/Aparato.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TiendaReparaciones.Core
+{
+    abstract class Aparato
+    {
+        protected Aparato(int numSerie, string modelo, int precioH)
+        {
+            this.numSerie = numSerie;
+            this.modelo = modelo;
+            this.precioH = precioH;
+        }
+
+        public int numSerie
+        {
+            get; set;
+        }
+
+        public string modelo
+        {
+            get; set;
+        }
+
+        public int precioH
+        {
+            get; set;
+        }
+
+        public override string ToString()
+        {
+            return "RADIO\nNum Serie: " + numSerie + "\nModelo: " + modelo + "\nPrecio por hora: " + precioH;
+        }
+
+        //CREAR LUEGO
+
+    }
+}
---
+++ TiendaReparaciones/Core/Aparatos/AdaptadorTDT.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TiendaReparaciones.Core
+{
+    class AdaptadorTDT : Aparato
+    {
+        public const int precioH = 5;
+
+        public AdaptadorTDT(int numSerie, string modelo, int tiempo)
+            : base(numSerie, modelo, precioH)
+        {
+            this.tiempo = tiempo;
+        }
+
+        public int tiempo
+        {
+            get; set;
+        }
+
+        public override string ToString()
+        {
+            return "ADAPTADOR TDT\n" + base.ToString() + "\Tiempo que graba: " + tiempo;
+        }
+    }
+}
