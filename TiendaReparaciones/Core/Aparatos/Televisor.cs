12b5a41057a3e94da0374eeffa79d988fb0fcb54
Fufeer <fralvarez@esei.uvigo.es>
2019-10-02 14:07:26 +0200

    Subir proyecto

---
+++ TiendaReparaciones/Core/Aparatos/Televisor.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TiendaReparaciones.Core
+{
+    class Televisor : Aparato
+    {
+        public const int precioH = 10;
+
+        public Televisor(int numSerie, string modelo, int pulgadas)
+            :base(numSerie, modelo, precioH)
+        {
+            this.pulgadas = pulgadas;
+        }
+
+        public int pulgadas
+        {
+            get; set;
+        }
+
+        public override string ToString()
+        {
+            return "TELEVISOR\n" + base.ToString() + "\nPulgadas: " + pulgadas;
+        }
+
+    }
+}
---
+++ TiendaReparaciones/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace TiendaReparaciones
+{
+    using Core;
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var r = new Radio(10, "sony", "am");
+            Console.WriteLine(r);
+        }
+    }
+}
